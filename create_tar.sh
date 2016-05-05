a709e803ffdcfd18763fdaff867de5de2f6c930d
Serghei Iakovlev <serghei@phalconphp.com>
2016-05-06 00:17:58 +0300

    Fix build tools

--- create_tar.sh
+++ create_tar.sh
@@ -6,4 +6,8 @@ else
     read -p "=> Please enter the version you wish to build: " VERSION
 fi
 
-cd .. && tar czf php-phalcon_$VERSION.orig.tar.gz php-phalcon --exclude=.git --exclude=.gitmodules --exclude=debian && cd php-phalcon
+cd ..
+
+tar czf php-phalcon_$VERSION.orig.tar.gz php-phalcon --exclude=.git --exclude=.gitignore --exclude=.gitmodules --exclude=debian
+
+cd php-phalcon
